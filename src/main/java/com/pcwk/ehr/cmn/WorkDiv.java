bf4640f02335563d499d025733be3b501e08880b
김신우 <thomastheship@gmail.com>
2026-06-30 17:52:03 +0900

    주석 수정

--- src/main/java/com/pcwk/ehr/cmn/WorkDiv.java
+++ src/main/java/com/pcwk/ehr/cmn/WorkDiv.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 /**
- * 공통 CRUD 계약 (v2). Service·Mapper가 동일 메서드명으로 구현.
+ * 공통 CRUD 계약. Service·Mapper가 동일 메서드명으로 구현.
  */
 public interface WorkDiv<T> {
 
